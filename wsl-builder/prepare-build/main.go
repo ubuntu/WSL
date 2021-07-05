7864b0aac8f985decbe13bad0b1a48d6f9864969
Didier Roche <didrocks@ubuntu.com>
2021-07-05 14:45:48 +0200

    Split wsl-builder in assets/build preparation
    We don’t have imagemagick on the windows machine and don’t need it.
    Split the two in 2 separate binaries and make needed adjustements.
    Compute correctly root directory now.
    
    Co-authored-by: Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>

--- wsl-builder/prepare-build/main.go
+++ wsl-builder/prepare-build/main.go
@@ -9,29 +9,14 @@ import (
 
 func main() {
 	rootCmd := &cobra.Command{
-		Use:   "wsl-builder COMMAND",
-		Short: "Update releases in WSL distribution info and assets from template",
-		Long: `This tool update releases from an incoming CSV file and generate
-		       every icons and xml files needed to build a WSL application`,
+		Use:   "prepare COMMAND",
+		Short: "Prepares the build tree before running msbuild",
+		Long: `This tool downloads the assets and prepares the build tree before
+		       running msbuild to build WSL application.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return nil
 		},
 	}
-
-	assetsCmd := &cobra.Command{
-		Use:   "assets CSV_FILE",
-		Short: "Update all releases in WSL distribution info and assets from template",
-		Long: `This tool update all releases from an incoming CSV file and generate
-		       every icons and xml files needed to build a WSL application`,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			if len(args) != 1 {
-				return errors.New("this command accepts exactly one CSV file")
-			}
-
-			return updateAssets(args[0])
-		},
-	}
-	rootCmd.AddCommand(assetsCmd)
 
 	buildGHMatrixCmd := &cobra.Command{
 		Use:   "build-github-matrix CSV_FILE",
@@ -50,7 +35,7 @@ func main() {
 
 	var noChecksum *bool
 	prepareBuildCmd := &cobra.Command{
-		Use:   "prepare-build ARTIFACTS_PATH WSL_ID ROOTFSES",
+		Use:   "prepare ARTIFACTS_PATH WSL_ID ROOTFSES",
 		Short: "Prepares the build source before calling msbuild",
 		Long: `This downloads the root file systems and prepares the build before
 		       building the appx bundle`,
