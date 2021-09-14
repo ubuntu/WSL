42898b659c7ebc6cfb0d9b301e5272dd8d7e9bff
didrocks <didrocks@users.noreply.github.com>
2021-09-14 11:33:08 +0000

    Refreshes releases and assets

--- meta/UbuntuPreview/DistroLauncher/DistributionInfo.h
+++ meta/UbuntuPreview/DistroLauncher/DistributionInfo.h
@@ -16,7 +16,7 @@ namespace DistributionInfo
     const std::wstring Name = L"UbuntuPreview";
 
     // The title bar for the console window while the distribution is installing.
-    const std::wstring WindowTitle = L"Ubuntu Preview";
+    const std::wstring WindowTitle = L"Ubuntu (Preview)";
 
     // Create and configure a user account.
     bool CreateUser(std::wstring_view userName);
