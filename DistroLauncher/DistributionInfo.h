d5073f05b5f29c50b22c8423ca75f20a7f147eb4
Didier Roche <didrocks@ubuntu.com>
2021-10-08 15:29:01 +0200

    Test name

--- DistroLauncher/DistributionInfo.h
+++ DistroLauncher/DistributionInfo.h
@@ -13,10 +13,10 @@ namespace DistributionInfo
     //
     // WARNING: This value must not change between versions of your app,
     // otherwise users upgrading from older versions will see launch failures.
-    const std::wstring Name = L"{{.WslID}}";
+    const std::wstring Name = L"MyDistroNameFromDistributionInfo";
 
     // The title bar for the console window while the distribution is installing.
-    const std::wstring WindowTitle = L"{{.FullName}}";
+    const std::wstring WindowTitle = L"My Window Title from Distribution info";
 
     // Create and configure a user account.
     bool CreateUser(std::wstring_view userName);
