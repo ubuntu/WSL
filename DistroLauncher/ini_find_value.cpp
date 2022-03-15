aeeaf3115339b8c98dfb0eb5d6c4bdd02651f268
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2022-03-15 13:40:55 -0300

    Wired systemd experimental support into main()
    - Implemented ini_find_value() to parse /etc/wsl.conf.
    - Returns true if it finds the section.key=*value* combination.
    - Returns false otherwise or if a parsing error is found.
    - Parsing error behavior is best-effort to mimic WSL.
    - It may diverge on unforeseen situations.
    - Minimal interference on upstream code (2 lines in DistroLauncher.cpp).

---
+++ DistroLauncher/ini_find_value.cpp
@@ -0,0 +1,95 @@
+#include "stdafx.h"
+#include <sstream>
+
+namespace Oobe::internal
+{
+    /// Trims both ends of a string in place.
+    inline void trim(std::wstring& str)
+    {
+        constexpr wchar_t* whitespaces = L" \t\n\r\f\v";
+
+        auto lastpos = str.find_last_not_of(whitespaces);
+        if (lastpos == std::wstring::npos) {
+            str.clear();
+            return;
+        }
+
+        str.erase(lastpos + 1);
+        str.erase(0, str.find_first_not_of(whitespaces));
+    }
+
+    /// Removes wrapping square brackets in place.
+    /// Returns false if the string didn't contain the wrapping bracket pair.
+    inline bool remove_surrounding_brackets(std::wstring& str)
+    {
+        if (str[0] != L'[') {
+            return false;
+        }
+        auto closingBracketPos = str.find_last_of(L']');
+        if (closingBracketPos == std::wstring::npos) {
+            return false;
+        }
+
+        str.erase(closingBracketPos);
+        str.erase(0, 1); // removes the first character only.
+        trim(str);
+        return true;
+    }
+
+    /// Returns true if the entry [section.key] is present in the [ini] file stream and its value contains the string
+    /// [valueContains]. This is intended to be called once, thus no caching.
+    bool ini_find_value(std::wistream& ini, std::wstring_view section, std::wstring_view key,
+                        std::wstring_view valueContains)
+    {
+        std::wstring line;
+        std::wistringstream splitter;
+        std::wstring currentSection, currentKey, value;
+        ini.seekg(0);
+        while (std::getline(ini, line)) {
+            // skip empty lines.
+            if (line.empty()) {
+                continue;
+            }
+            trim(line);
+            switch (line[0]) {
+            case L'#': // skip comments.
+                break;
+
+            case L'[':
+                // ill-formed section header breaks parsing.
+                if (!remove_surrounding_brackets(line)) {
+                    return false;
+                }
+                currentSection = line;
+                break;
+
+            default: // it has to be a key=value line.
+                // If there was no single '=' character in this line, most probably there is a syntax error and the
+                // configuration file is ill-formed. Thus, WSL would have ignored the rest of the file, so it doesn't
+                // matter whether we would find systemd activation line or not after this point or not. WSL current
+                // implementation stops parsing the file after a parsing error is found, yet it performs the
+                // configuration steps of the previously validated entries. There might be billions of other ways to
+                // break the syntax. It's probably not worthy attempting to prevent them all.
+                if (line.find_first_of(L'=') == std::wstring::npos) {
+                    return false;
+                }
+                splitter.clear();
+                splitter.str(line);
+                std::getline(splitter, currentKey, L'=');
+                std::getline(splitter, value, L'=');
+                trim(currentKey);
+                trim(value);
+
+                // The only possible way to return true from this function is finding the key=*valueContains* definition
+                // we are looking for inside the proper section.
+                if (currentSection.compare(section) == 0 && currentKey.compare(key) == 0 &&
+                    value.find(valueContains) != std::wstring::npos) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    } // bool ini_find_value()
+
+} // Oobe::internal
