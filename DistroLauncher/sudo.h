7e323903fb35f63378d69cc55f3f5ed0a83c39d1
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2022-07-14 13:08:53 +0200

    Made sudo more testable

--- DistroLauncher/sudo.h
+++ DistroLauncher/sudo.h
@@ -41,7 +41,7 @@
  *  The last one should be avoided.
  *
  *                             MONADIC INTERFACE (UNSAFE)
- * 
+ *
  *  This interface uses `and_then` and `or_else` in order to perform actions.
  *  Example:
  *  ```
@@ -59,7 +59,7 @@
  *  and release the mutex, because the and_then can be chained.
  *
  *                           WSL API INTERFACE (SAFE)
- * 
+ *
  * This interface is modelled after WSL's windows API:
  * ```
  * DWORD exitCode = 0;
@@ -68,7 +68,7 @@
  * ```
  *
  *                        SCOPE LOCK INTERFACE (UNSAFE)
- * 
+ *
  * Works like a scope lock. Do not use unless you have a good reason to!
  * ```
  * {
@@ -88,76 +88,160 @@
  *     .transform([] (auto why) { log_error(why); });
  * ```
  */
-class Sudo
+
+namespace SudoInternals
 {
-  public:
-    enum class Status
+    // Templated for testing/dependency injection reasons, always use the alias at the end of the file
+    template <typename MutexType> class SudoInterface
     {
-        OK = 0,
-        FAILED_MUTEX,
-        FAILED_GET_USER,
-        FAILED_SET_ROOT,
-        INACTIVE
+      public:
+        enum class Status
+        {
+            OK = 0,
+            FAILED_MUTEX,
+            FAILED_GET_USER,
+            FAILED_SET_ROOT,
+            INACTIVE
+        };
+
+        SudoInterface() noexcept;
+        SudoInterface(SudoInterface&) = delete;
+        SudoInterface::SudoInterface(SudoInterface&& other) noexcept;
+        ~SudoInterface();
+
+        SudoInterface& operator=(SudoInterface& other) = delete;
+
+        bool ok() const noexcept
+        {
+            return status == Status::OK;
+        }
+
+        Status why() const noexcept
+        {
+            return status;
+        }
+
+        // Monadic interface
+        template <typename Callable> SudoInterface&& and_then(Callable&& f)
+        {
+            if (ok()) {
+                safe_execute(std::forward<Callable>(f), [&]() noexcept { reset_user(); });
+            }
+            return std::move(*this);
+        }
+
+        template <typename Callable> SudoInterface&& or_else(Callable&& f)
+        {
+            // Choosing overload
+            constexpr bool f_status = std::is_invocable_v<Callable, Status>;
+            constexpr bool f_void = !f_status && std::is_invocable_v<Callable>;
+
+            static_assert(f_void || f_status, "Callable must be ether 'T function()' or 'T function(Sudo::Status)'");
+
+            if (!ok()) {
+                if constexpr (f_void) {
+                    f();
+                }
+                if constexpr (f_status) {
+                    f(why());
+                }
+            }
+            return std::move(*this);
+        }
+
+        // wsl API interface
+        static HRESULT SudoInterface::WslLaunchInteractive(PCWSTR command, BOOL useCurrentWorkingDirectory,
+                                                           DWORD* exitCode);
+        static HRESULT SudoInterface::WslLaunch(PCWSTR command, BOOL useCurrentWorkingDirectory, HANDLE stdIn,
+                                                HANDLE stdOut, HANDLE stdErr, HANDLE* process);
+
+      private:
+        static MutexType sudo_mutex;
+
+        void reset_user() noexcept;
+
+        typename MutexType::Lock mutex_lock{};
+
+        Status status = Status::INACTIVE;
+        ULONG default_user_id = 0;
+        WSL_DISTRIBUTION_FLAGS wsl_distribution_flags{};
     };
 
-    Sudo() noexcept;
-    Sudo(Sudo&) = delete;
-    Sudo::Sudo(Sudo&& other) noexcept;
-    ~Sudo();
-
-    Sudo& operator=(Sudo& other) = delete;
-
-    bool ok() const noexcept
+    template <typename MutexType> SudoInterface<MutexType>::SudoInterface() noexcept
     {
-        return status == Status::OK;
+        auto lock = sudo_mutex.lock();
+        if (!lock.ok()) {
+            status = Status::FAILED_MUTEX;
+            return;
+        }
+
+        if (const HRESULT hr = Oobe::WslGetDefaultUserAndFlags(default_user_id, wsl_distribution_flags); FAILED(hr)) {
+            status = Status::FAILED_GET_USER;
+            return;
+        }
+
+        constexpr ULONG root_uid = 1;
+        if (const HRESULT hr = g_wslApi.WslConfigureDistribution(root_uid, wsl_distribution_flags); FAILED(hr)) {
+            status = Status::FAILED_SET_ROOT;
+            return;
+        }
+
+        mutex_lock = std::move(lock);
+        status = Status::OK;
     }
 
-    Status why() const noexcept
+    template <typename MutexType> SudoInterface<MutexType>::SudoInterface(SudoInterface&& other) noexcept
     {
-        return status;
+        if (this != &other) {
+            mutex_lock = std::exchange(other.mutex_lock, {});
+            default_user_id = std::exchange(other.default_user_id, 0);
+            wsl_distribution_flags = std::exchange(other.wsl_distribution_flags, {});
+            status = std::exchange(other.status, Status::INACTIVE);
+        }
     }
 
-    // Monadic interface
-    template <typename Callable> Sudo&& and_then(Callable&& f)
+    template <typename MutexType> void SudoInterface<MutexType>::reset_user() noexcept
     {
         if (ok()) {
-            safe_execute(std::forward<Callable>(f), [&] () noexcept { reset_user(); });
+            g_wslApi.WslConfigureDistribution(default_user_id, wsl_distribution_flags);
+            MutexType::Lock dummy_lock;
+            std::swap(mutex_lock, dummy_lock);
+            status = Status::INACTIVE;
         }
-        return std::move(*this);
     }
 
-    template <typename Callable> Sudo&& or_else(Callable&& f)
+    template <typename MutexType> SudoInterface<MutexType>::~SudoInterface()
     {
-        // Choosing overload
-        constexpr bool f_status = std::is_invocable_v<Callable, Status>;
-        constexpr bool f_void = !f_status && std::is_invocable_v<Callable>;
-
-        static_assert(f_void || f_status, "Callable must be ether 'T function()' or 'T function(Sudo::Status)'");
-
-        if (!ok()) {
-            if constexpr (f_void) {
-                f();
-            }
-            if constexpr (f_status) {
-                f(why());
-            }
-        }
-        return std::move(*this);
+        reset_user();
     }
 
-    // wsl API interface
-    static HRESULT Sudo::WslLaunchInteractive(PCWSTR command, BOOL useCurrentWorkingDirectory, DWORD* exitCode);
-    static HRESULT Sudo::WslLaunch(PCWSTR command, BOOL useCurrentWorkingDirectory, HANDLE stdIn, HANDLE stdOut,
-                                   HANDLE stdErr, HANDLE* process);
+    template <typename MutexType>
+    HRESULT SudoInterface<MutexType>::WslLaunchInteractive(PCWSTR command, BOOL useCurrentWorkingDirectory,
+                                                           DWORD* exitCode)
+    {
+        HRESULT hr = S_FALSE;
+        SudoInterface()
+          .and_then([&]() { hr = g_wslApi.WslLaunchInteractive(command, useCurrentWorkingDirectory, exitCode); })
+          .or_else([&] { hr = S_FALSE; });
 
-  private:
-    static NamedMutex sudo_mutex;
+        return hr;
+    }
 
-    void reset_user() noexcept;
+    template <typename MutexType>
+    HRESULT SudoInterface<MutexType>::WslLaunch(PCWSTR command, BOOL useCurrentWorkingDirectory, HANDLE stdIn,
+                                                HANDLE stdOut, HANDLE stdErr, HANDLE* process)
+    {
+        HRESULT hr = S_FALSE;
+        SudoInterface()
+          .and_then([&]() noexcept {
+              hr = g_wslApi.WslLaunch(command, useCurrentWorkingDirectory, stdIn, stdOut, stdErr, process);
+          })
+          .or_else([&] { hr = S_FALSE; });
 
-    NamedMutex::Lock mutex_lock{};
+        return hr;
+    }
 
-    Status status = Status::INACTIVE;
-    ULONG default_user_id = 0;
-    WSL_DISTRIBUTION_FLAGS wsl_distribution_flags{};
-};
+}
+
+using Sudo = SudoInternals::SudoInterface<NamedMutex>;
+inline NamedMutex Sudo::sudo_mutex(L"root-user", true);
