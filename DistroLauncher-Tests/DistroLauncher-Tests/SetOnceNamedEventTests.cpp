73c284d79be9a8ecf7cd6ace28c137a96aa33451
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2022-07-25 08:49:39 -0300

    Fix whitespace

--- DistroLauncher-Tests/DistroLauncher-Tests/SetOnceNamedEventTests.cpp
+++ DistroLauncher-Tests/DistroLauncher-Tests/SetOnceNamedEventTests.cpp
@@ -7,7 +7,7 @@ namespace Win32Utils
     TEST(SetOnceNamedEvent, ValidUntilSet)
     {
         auto now = std::chrono::system_clock::now().time_since_epoch();
-        std::wstring name = L"a-global-unique-name-for-today"  + std::to_wstring(now.count());
+        std::wstring name = L"a-global-unique-name-for-today" + std::to_wstring(now.count());
         SetOnceNamedEvent event{name.c_str()};
         EXPECT_TRUE(event.isValid());
         EXPECT_TRUE(event.set());
