c84dd8021c8f524351fe6738308942aece18a4f7
didrocks <didrocks@users.noreply.github.com>
2022-08-13 02:40:24 +0000

    Refreshes releases and assets

--- meta/Ubuntu20.04LTS/generated/DistroLauncher/DistributionInfo.h
+++ meta/Ubuntu20.04LTS/generated/DistroLauncher/DistributionInfo.h
@@ -16,7 +16,7 @@ namespace DistributionInfo
     const std::wstring Name = L"Ubuntu-20.04";
 
     // The title bar for the console window while the distribution is installing.
-    const std::wstring WindowTitle = L"Ubuntu 20.04.4 LTS";
+    const std::wstring WindowTitle = L"Ubuntu 20.04.5 LTS";
 
     // Create and configure a user account.
     bool CreateUser(std::wstring_view userName);
