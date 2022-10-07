6a59420059c0954ba1dbc90ef16ae563a8453a8c
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2022-10-07 08:33:13 -0300

    Named 123 constant for clarity.

--- DistroLauncher/Application.h
+++ DistroLauncher/Application.h
@@ -21,6 +21,7 @@
 
 namespace Oobe
 {
+    static constexpr int EXIT_OOBE_NO_FALLBACK = 123;
     /// Provides the high level API through which the controllers and services are integrated to run (or not) the OOBE
     /// according to the hardware platform capabilities and the result of the command line parsing. Platform specific
     /// code is provided by the Strategy template type.
@@ -85,7 +86,7 @@ namespace Oobe
                 wprintf(L"Installer did not complete successfully.\n");
                 Helpers::PrintErrorMessage(hr);
                 if (appConfig().mustSkipFallback) {
-                    exit(123);
+                    exit(EXIT_OOBE_NO_FALLBACK);
                 }
                 wprintf(L"Applying fallback method.\n");
             }
