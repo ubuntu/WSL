b0894ee179a46d14ebf4edc7a67ae111d7fc178d
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2022-10-10 14:07:40 -0300

    Enables setting local certificate thumbprint
    + documents better the CLI options

--- e2e/pre-e2e-testing.ps1
+++ e2e/pre-e2e-testing.ps1
@@ -3,6 +3,7 @@
 # Parameters:
 #   AppID = UbuntuPreview, Ubuntu22.04LTS etc etc.
 #   RootFsX64 = URL for the rootfs used for this build
+#   CertificateThumbprint = Optional: Thumbprint of the local certificate to use when signing the package.
 #
 # Preconditions:
 #   PWD=$env:LAUNCHER_REPO_ROOT
@@ -12,10 +13,12 @@
 #   e2e testing enabled appx is installed.
 
 param (
-    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [Parameter(Mandatory=$true, HelpMessage="UbuntuPreview, Ubuntu22.04LTS etc etc.")]
     [string]$AppID,
-    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-    [string]$RootFsX64
+    [Parameter(Mandatory=$true, HelpMessage="the URL to fetch the rootfs from used for this build")]
+    [string]$RootFsX64,
+    [Parameter(Mandatory=$false, HelpMessage="Optional: Thumbprint of the local certificate to use when signing the package.")]
+    [string]$CertificateThumbprint
 )
 
 function FailFast($exitCode){
@@ -39,15 +42,26 @@ FailFast ($LastExitCode)
 # Remove ARM64 contents out of the solution file
 Set-Content -Path "DistroLauncher.sln" -Value (get-content -Path "DistroLauncher.sln" | Select-String -Pattern 'ARM64' -NotMatch)
 
-# Actions should inherit Secrets even in the case of self-hosted runners. Not touching it here.
+# Actions should inherit Secrets even in the case of self-hosted runners. Not touching it here, unless another thumbprint is set.
+# Useful for local development.
+If ( -Not [string]::IsNullOrEmpty($CertificateThumbprint)) {
+    Write-Output "Modifying signing certificate"
+    $vcxprojFile = "DistroLauncher-Appx/DistroLauncher-Appx.vcxproj"
+    $thumbprintEntry = "<PackageCertificateThumbprint>@</PackageCertificateThumbprint>"
+    $wildCard = $thumbprintEntry.replace("@", ".*")
+    $replacement = $thumbprintEntry.replace("@", $CertificateThumbprint)
+
+    [regex]::replace($(Get-Content -Path $vcxprojFile), $wildCard, $replacement) | Set-Content -Path $vcxprojFile
+}
 
 # Copies the ui-driver.props file to force MSBuild to bundle that binary inside the appx.
 cp ./e2e/ui-driver/ui-driver.props DistroLauncher-Appx/
 
 # Build the appx bundle for E2E testing
-msbuild .\DistroLauncher.sln /t:Build /m /nr:false /p:Configuration=Debug /p:AppxBundle=Always /p:AppxBundlePlatforms="x64" /p:UapAppxPackageBuildMode=SideloadOnly -verbosity:normal /p:OOBE_E2E_TESTING=True
+msbuild ./DistroLauncher.sln /t:Build /m /nr:false /p:Configuration=Debug /p:AppxBundle=Always /p:AppxBundlePlatforms="x64" /p:UapAppxPackageBuildMode=SideloadOnly -verbosity:normal /p:OOBE_E2E_TESTING=True
 FailFast ($LastExitCode)
 
 # Install the appx
-./AppPackages/Ubuntu/*/Install.ps1 -Force
+$OutputDir = $(Get-ChildItem -Directory .\AppPackages\Ubuntu\ | Sort-Object -Property {$_.LastWriteTime} -Descending)[0].Name
+Invoke-Expression "./AppPackages/Ubuntu/$OutputDir/Install.ps1 -Force"
 FailFast ($LastExitCode)
