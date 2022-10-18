c9a9a32e350d3d4a620622de4c29ade218eca9a7
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2022-10-18 09:24:14 +0200

    Added distroName as argument to default experience functions

--- e2e/test-runner/default_experience_test.go
+++ e2e/test-runner/default_experience_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 // Parses the output of wsl -l -v to find the STATE of the current distro
-func assertGetDistroState(t *Tester) string {
+func assertGetDistroState(t *Tester, distro string) string {
 	// wsl -l -v outputs UTF-16 (See https://github.com/microsoft/WSL/issues/4607)
 	// We use $env:WSL_UTF8=1 to prevent this (Available from 0.64.0 onwards https://github.com/microsoft/WSL/releases/tag/0.64.0)
 	str := t.AssertOsCommand("powershell.exe", "-noninteractive", "-nologo", "-noprofile", "-command",
@@ -26,7 +26,7 @@ func assertGetDistroState(t *Tester) string {
 		if columns[idx] == "*" {
 			idx++
 		}
-		if columns[idx] == *distroName {
+		if columns[idx] == distro {
 			return columns[idx+1]
 		}
 	}
@@ -35,11 +35,11 @@ func assertGetDistroState(t *Tester) string {
 
 // Waits until the current state (fromState) transitions into toState.
 // Fails if any other state is reached
-func assertWaitStateTransition(tester *Tester, outbuff *bytes.Buffer, fromState string, toState string) string {
-	state := assertGetDistroState(tester)
+func assertWaitStateTransition(tester *Tester, outbuff *bytes.Buffer, distro string, fromState string, toState string) string {
+	state := assertGetDistroState(tester, distro)
 	for state == fromState {
 		time.Sleep(1 * time.Second)
-		state = assertGetDistroState(tester)
+		state = assertGetDistroState(tester, distro)
 	}
 	if state != toState {
 		tester.Logf("In transition from '%s' to '%s': Unexpected state '%s'", fromState, toState, state)
@@ -68,13 +68,13 @@ func TestDefaultExperience(t *testing.T) {
 	cmd.Start()
 
 	// Completing installation
-	assertWaitStateTransition(&tester, &out, "DistroNotFound", "Installing")
-	assertWaitStateTransition(&tester, &out, "Installing", "Running")
-	assertWaitStateTransition(&tester, &out, "Running", "Stopped")
+	assertWaitStateTransition(&tester, &out, *distroName, "DistroNotFound", "Installing")
+	assertWaitStateTransition(&tester, &out, *distroName, "Installing", "Running")
+	assertWaitStateTransition(&tester, &out, *distroName, "Running", "Stopped")
 
 	// Ensuring proper installation
 	if !strings.Contains(out.String(), "Installation successful!") { // TODO: Change this to parse MOTD
-		tester.Logf("Status: %s", assertGetDistroState(&tester))
+		tester.Logf("State: %s", assertGetDistroState(&tester, *distroName))
 		tester.Logf("Output: %s", out.String())
 		tester.Fatal("Distro was shut down without finishing install")
 	}
