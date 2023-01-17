69fa141ee68e4ddb8333fb9109d0834f9488922a
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2023-01-17 09:52:51 +0100

    Implement release-info module to print WSL release information
    Some code had to be factored out of prepare-build so as to have a single
    implementation of the rootfs URL generator.

--- wsl-builder/prepare-build/buildGHMatrix.go
+++ wsl-builder/prepare-build/buildGHMatrix.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"path/filepath"
 	"strings"
 
 	"github.com/ubuntu/wsl/wsl-builder/common"
@@ -30,28 +29,15 @@ func buildGHMatrix(csvPath, metaPath string) error {
 			continue
 		}
 
-		var rootfses string
-		for i, arch := range []string{"amd64", "arm64"} {
-			t := ","
-			if i == 0 {
-				t = ""
-			}
-			// Currently only Kinetic (22.10) and later are published to "https://cloud-images.ubuntu.com/wsl/"
-			codeNameSubUri := r.CodeName
-			imageBaseName := fmt.Sprintf("%s-server-cloudimg", r.CodeName)
-			if strings.Compare(r.BuildVersion, "2210") >= 0 {
-				codeNameSubUri = filepath.Join("wsl", r.CodeName)
-				// The image base name scheme also changed.
-				imageBaseName = fmt.Sprintf("ubuntu-%s-wsl", r.CodeName)
-			}
-
-			t += fmt.Sprintf("https://cloud-images.ubuntu.com/%s/current/%s-%s-wsl.rootfs.tar.gz::%s", codeNameSubUri, imageBaseName, arch, arch)
-			rootfses += t
+		rootfsUrls := []string{}
+		for _, arch := range []string{"amd64", "arm64"} {
+			urlAndArch := fmt.Sprintf("%s::%s", common.RootfsUrl(r, arch), arch)
+			rootfsUrls = append(rootfsUrls, urlAndArch)
 		}
 
 		allBuilds = append(allBuilds, matrixElem{
 			AppID:            r.AppID,
-			Rootfses:         rootfses,
+			Rootfses:         strings.Join(rootfsUrls, ","),
 			RootfsesChecksum: "yes",
 			Upload:           "yes",
 		})
---
+++ wsl-builder/release-info/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"os"
+
+	"github.com/0xrawsec/golang-utils/log"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var f columns
+	rootCmd := &cobra.Command{
+		Use:   "release-info CSV_FILE [DISTRO...] [--app-id] [--full-name] [--code-name] [--launcher] [--rootfs Arch] [--short]",
+		Short: "Print informantion regarding a release",
+		Long: `This tool shows information about any WSL release. Write no distro to see all of them.
+Fields are always in the same order, regardless of the order the flags are passed in.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				return errors.New("this command needs at least one argument (CSV_FILE)")
+			}
+
+			f.rootfs = new(bool)
+			if len(*f.rootfsArch) != 0 {
+				*f.rootfs = true
+			}
+
+			exitCode := writeReleaseInfo(args[0], args[1:], f)
+			os.Exit(exitCode)
+			return nil // Returning an error causes Cobrea to print its help
+		},
+	}
+
+	f.appId = rootCmd.Flags().Bool("app-id", false, "Display the App ID as shown in the store.")
+	f.fullName = rootCmd.Flags().Bool("full-name", false, "Display the App FullName as shown in your machine.")
+	f.launcher = rootCmd.Flags().Bool("launcher", false, "Display the launcher executable name.")
+	f.codeName = rootCmd.Flags().Bool("code-name", false, "Display the code name, i.e. the distro's adjective.")
+	f.rootfsArch = rootCmd.Flags().String("rootfs", "", "Display the URL where the rootfs can be downloaded.")
+	f.short = rootCmd.Flags().BoolP("short", "s", false, "Don't display the table header.")
+
+	err := rootCmd.Execute()
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+}
