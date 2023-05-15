a7b052c0d94104e53ec43e050610fa33d9828e36
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-05-15 12:56:14 +0200

    Remove systemd-modules-load.service from expected failed services

--- e2e/launchertester/test_cases.go
+++ e2e/launchertester/test_cases.go
@@ -130,7 +130,7 @@ func testSystemdUnits(t *testing.T) { //nolint: thelper, this is a test
 	terminateDistro(t)
 
 	distroNameToFailedUnits := map[string][]string{
-		"Ubuntu-18.04":   {"user@0.service", "atd.service", "systemd-modules-load.service"},
+		"Ubuntu-18.04":   {"user@0.service", "atd.service"},
 		"Ubuntu-20.04":   {"user@0.service", "atd.service", "systemd-remount-fs.service", "multipathd.socket"},
 		"Ubuntu-22.04":   {"user@0.service"},
 		"Ubuntu":         {"user@0.service"},
