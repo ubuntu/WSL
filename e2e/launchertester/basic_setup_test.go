d0868721177393656ef1459a87bfdc2185924f0c
Carlos <carlos.santanadeoliveira@canonical.com>
2024-01-29 15:26:38 -0300

    Remove the awereness of a OOBE GUI

--- e2e/launchertester/basic_setup_test.go
+++ e2e/launchertester/basic_setup_test.go
@@ -9,33 +9,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TestBasicSetupGUI runs a battery of assertions after installing with the graphical OOBE.
-func TestBasicSetupGUI(t *testing.T) {
-	wslSetup(t)
-
-	ctx, cancel := context.WithTimeout(context.Background(), installTimeout)
-	defer cancel()
-	out, err := launcherCommand(ctx, "install", "--ui=gui").CombinedOutput()
-	require.NoErrorf(t, err, "Unexpected error installing: %s\nLogs:%s", out, subiquityLogs(t))
-	// TODO: check with Carlos if this is necessary
-	require.NotEmpty(t, out, "Failed to install the distro: No output produced. Logs:%s", subiquityLogs(t))
-
-	testCases := map[string]func(t *testing.T){
-		"UserNotRoot":             testUserNotRoot,
-		"LanguagePacksMarked":     testLanguagePacksMarked,
-		"SystemdEnabled":          testSystemdEnabled,
-		"SystemdUnits":            testSystemdUnits,
-		"CorrectUpgradePolicy":    testCorrectUpgradePolicy,
-		"UpgradePolicyIdempotent": testUpgradePolicyIdempotent,
-		"InteropIsEnabled":        testInteropIsEnabled,
-		"HelpFlag":                testHelpFlag,
-	}
-
-	for name, tc := range testCases {
-		t.Run(name, tc)
-	}
-}
-
 // TestBasicSetupGUI runs a battery of assertions after installing with WSL's default OOBE.
 func TestBasicSetupFallbackUI(t *testing.T) {
 	wslSetup(t)
@@ -43,7 +16,7 @@ func TestBasicSetupFallbackUI(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
 	defer cancel()
 	// TODO: try to inject user/password to stdin to avoid --root arg.
-	out, err := launcherCommand(ctx, "install", "--root", "--ui=none").CombinedOutput() // Installing as root to avoid Stdin
+	out, err := launcherCommand(ctx, "install", "--root").CombinedOutput() // Installing as root to avoid Stdin
 	require.NoErrorf(t, err, "Unexpected error installing: %s\n%v", out, err)
 	// TODO: check with Carlos if this is necessary
 	require.NotEmpty(t, out, "Failed to install the distro: No output produced.")
