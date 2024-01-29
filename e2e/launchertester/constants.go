d3bd8da29d623d1db4bd70040550890f31364f7a
Carlos <carlos.santanadeoliveira@canonical.com>
2024-01-29 15:26:05 -0300

    No need to know launcher repo root
    no subiquity logs to assess
    no build steps

--- e2e/launchertester/constants.go
+++ e2e/launchertester/constants.go
@@ -3,4 +3,3 @@ package launchertester
 // Defaults choosen based on the default state of the repository on git-clone.
 const DefaultDistroName = "UbuntuDev.WslID.Dev"
 const DefaultLauncherName = "UbuntuDev.LauncherName.Dev.exe"
-const LauncherRepoEnvVar = "LAUNCHER_REPO_ROOT"
