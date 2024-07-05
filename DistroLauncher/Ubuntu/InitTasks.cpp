49994b7c69ed1817408f9675de7c4bc99d04094c
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2024-07-05 17:13:43 -0300

    Ask for forgiveness, not permission
    When approaching cloud-init, run it unconditionally
    Added a not-so-beautiful trick to avoid printing
    /bin/bash: error 1: cloud-init command not found.
    Finally, if it doesn't exit 0, show the long status
    
    cloud-init status --long.

--- DistroLauncher/Ubuntu/InitTasks.cpp
+++ DistroLauncher/Ubuntu/InitTasks.cpp
@@ -39,29 +39,40 @@ namespace {
 void waitForInitTasks(WslApiLoader& api) {
   _putws(L"Checking for initialization tasks...\n");
 
-  DWORD exitCode_ = -1;
-  if (auto hr = api.WslLaunchInteractive(L"which -s cloud-init", FALSE, &exitCode_); FAILED(hr)) {
+  DWORD exitCode = -1;
+  // Try running cloud-init unconditionally, but avoid printing to console if it doesn't exist.
+  auto hr = api.WslLaunchInteractive(
+      L"function command_not_found_handle() { return 127; }; cloud-init status --wait", FALSE,
+      &exitCode);
+  if (FAILED(hr)) {
     Helpers::PrintErrorMessage(hr);
-    return;
-  }
-  if (exitCode_ != 0) {
-    _putws(L"CheckInitTasks: this release doesn't support initialization tasks.\n");
     return;
   }
 
-  exitCode_ = -1;
-  if (auto hr = api.WslLaunchInteractive(L"cloud-init status --wait", FALSE, &exitCode_);
-      FAILED(hr)) {
-    Helpers::PrintErrorMessage(hr);
-    return;
-  }
-  // 0 for "success" or 2 for "recoverable error" - sometimes we get that while status shows "done".
+  // 127 for command not found, 0 for "success" or 2 for "recoverable error" - sometimes we get that
+  // while status shows "done".
   // https://cloudinit.readthedocs.io/en/latest/explanation/failure_states.html#cloud-init-error-codes
   // If that's the case we should inspect the system to check whether we should skip or proceed with
   // user creation.
-  if (exitCode_ != 0 && exitCode_ != 2) {
-    wprintf(L"CheckInitTasks: failed with exit code: %u\n", exitCode_);
+  switch (exitCode) {
+    case 0: {
+      return;
+    }
+    case 127: {
+      _putws(L"INFO: this release doesn't support initialization tasks.\n");
+      return;
+    }
+    case 2: {
+      _putws(L"WARNING: initialization tasks partially succeeded, see below:");
+      break;
+    }
+    default: {
+      wprintf(L"ERROR: initialization failed with exit code: %u\n", exitCode);
+      break;
+    }
   }
+  // We don't really care if the command below fails, it's just informative.
+  api.WslLaunchInteractive(L"cloud-init status --long", FALSE, &exitCode);
 }
 
 // Groups the pieces of information from a single user entry in the passwd database we care about.
