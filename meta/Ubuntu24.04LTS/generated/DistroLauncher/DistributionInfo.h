fcce02f890682ee18818139c5c392ea28d1cbc7f
didrocks <didrocks@users.noreply.github.com>
2024-07-27 01:48:56 +0000

    Refreshes releases and assets

--- meta/Ubuntu24.04LTS/generated/DistroLauncher/DistributionInfo.h
+++ meta/Ubuntu24.04LTS/generated/DistroLauncher/DistributionInfo.h
@@ -16,7 +16,7 @@ namespace DistributionInfo
     const std::wstring Name = L"Ubuntu-24.04";
 
     // The title bar for the console window while the distribution is installing.
-    const std::wstring WindowTitle = L"Ubuntu 24.04 LTS";
+    const std::wstring WindowTitle = L"Ubuntu 24.04.1 LTS";
 
     // Create and configure a user account.
     bool CreateUser(std::wstring_view userName);
